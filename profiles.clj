56e25849860957d623d771a082b45e533ff3038d
Daniel Hosterman <daniel.hosterman@gmail.com>
2015-06-23 11:28:18 -0400

    Add .lein/profiles.clj to dotfiles for Clojure development.

---
+++ profiles.clj
@@ -0,0 +1,9 @@
+{:user {:plugins [[lein-bikeshed "0.2.0"]
+                  [jonase/eastwood "0.2.1"]
+                  [lein-kibit "0.1.2"]
+                  [lein-ancient "0.6.7"]
+                  [lein-try "0.4.3"]
+                  [lein-cljfmt "0.1.10"]
+                  [lein-midje "3.1.3"]]
+        :dependencies [[spyscope "0.1.3"]]
+        :injections   [(require 'spyscope.core)]}}
